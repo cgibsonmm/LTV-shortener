c855d5cb50edb80844d20c4fe024b397a620feef
corey gibson <cgibsonmm@gmail.com>
2021-12-13 16:33:11 -0500

    testing out basic encoding and decoding base_62

--- spec/models/id_parser_spec.rb
+++ spec/models/id_parser_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 include IdParser
 
-RSpec.describe IdParser, type: :model do
+RSpec.describe IdParser do
   it 'creates a short url' do
     expect(convert_id(0)).to eq(0)
     expect(convert_id(1)).to eq('1')
