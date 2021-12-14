b8936366dea787cb70d7502eab13c0ef6f67679d
corey gibson <cgibsonmm@gmail.com>
2021-12-13 23:09:45 -0500

    chose a url verication method, I spent a lot of time looking into diffrent regex and URI, I decided to go with a modified URI approach(there is still a better approach I think though).

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -1,15 +1,10 @@
 class ShortUrlsController < ApplicationController
-
   # Since we're working on an API, we don't have authenticity tokens
   skip_before_action :verify_authenticity_token
 
-  def index
-  end
+  def index; end
 
-  def create
-  end
+  def create; end
 
-  def show
-  end
-
+  def show; end
 end
--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -4,14 +4,30 @@ class ShortUrl < ApplicationRecord
 
   # struggled for awhile to decide what to do until I saw this.
   # CHARACTERS = [*'0'..'9', *'a'..'z', *'A'..'Z'].freeze
-
+  validates :full_url, presence: true
   validate :validate_full_url
 
-  def short_code; end
+  def short_code
+    return nil unless id
+
+    convert_id(id)
+  end
 
   def update_title!; end
 
+  scope :find_by_short_code, ->(code) { where('id', decode_id(code)).first }
+
   private
 
-  def validate_full_url; end
+  def validate_full_url
+    # This is a not perfect solution looks to brittle and would not work on URL such as https://https://www.foo.org
+    # inspiration from https://stackoverflow.com/questions/35468284/ruby-rails-model-url-validation
+    uri = URI::DEFAULT_PARSER.make_regexp(%w[http https])
+    errors.add :full_url, :invalid, message: 'is not a valid url' unless uri.match(full_url)
+
+    # I would also consider using open-uri. Chris Oliver talks of this approach
+    # which I like but I think it would
+    # defeat the purpose of using the background job to find the title later
+    # https://gorails.com/forum/best-way-to-validate-url
+  end
 end
